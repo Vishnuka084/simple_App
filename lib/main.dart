83188edce9f0d3ba7473549e0d8b323b2468b03d
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-02-06 15:45:56 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "new app ",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Flutter App"),
+        ),
+      ),
+    );
+  }
+}
